56567bd5a27243760d656afdf600b1de3bff1f9b
pratham022 <pthombre657@gmail.com>
2021-11-01 16:12:06 +0530

    Create express and nodejs server

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res)  => {
+    res.send('Hello world from server');
+});
+
+app.get('/about', (req, res)  => {
+    res.send('Hello world ABOUT from server');
+});
+
+app.get('/contact', (req, res)  => {
+    res.send('Hello world CONTACT from server');
+});
+
+app.get('/signin', (req, res)  => {
+    res.send('Hello world SIGN IN from server');
+});
+
+app.get('/signup', (req, res)  => {
+    res.send('Hello world SIGN UP from server');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running at PORT 3000');
+})
+// console.log('Hello to the console');

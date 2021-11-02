60af2f3975ac01e1fc3d123e092ab4ae48d5823d
pratham022 <pthombre657@gmail.com>
2021-11-02 17:02:25 +0530

    Save User data in Atlas using Async Await

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -8,6 +8,7 @@ router.get('/', (req, res)  => {
     res.send('Hello world from server');
 });
 
+// Using Promises
 router.post('/register', (req, res) => {
    
     const { name, email, phone, work, password, cpassword } = req.body;
@@ -32,4 +33,29 @@ router.post('/register', (req, res) => {
     .catch(err => {console.log(err)})
 });
 
+
+// Using Async Await
+router.post('/register', async(req, res) => {
+   
+    const { name, email, phone, work, password, cpassword } = req.body;
+
+    if(!name || !email || !phone || !work || !password || !cpassword) {
+        return res.status(422).json({error: "Please fill out all the fields!"})
+    }
+
+    try {
+        const userExist = await User.findOne({email: email});
+        if(userExist)
+            return res.status(422).json({error: "Email already exists!"});
+    
+        const user = new User({
+            name, email, phone, work, password, cpassword
+        });
+
+        await user.save();
+        res.status(201).json({message: "User Registered Successfully"});
+    } catch(err) {
+        console.log(err);
+    }
+});
 module.exports = router;

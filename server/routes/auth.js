48cd9bf63a7523cb1ffb8ccecdfa5799cf4c44b1
pratham022 <pthombre657@gmail.com>
2021-11-02 16:36:09 +0530

    Save User data in Atlas using Promises

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,13 +1,35 @@
 const express = require('express');
 const router = express.Router();
 
+require('../db/conn');
+const User = require('../models/userSchema');
+
 router.get('/', (req, res)  => {
     res.send('Hello world from server');
 });
 
 router.post('/register', (req, res) => {
-    console.log(req.body);
-    res.json({message: req.body})
+   
+    const { name, email, phone, work, password, cpassword } = req.body;
+    if(!name || !email || !phone || !work || !password || !cpassword) {
+        return res.status(422).json({error: "Please fill out all the fields!"})
+    }
+    User.findOne({email: email})
+    .then((userExist) => {
+        if(userExist)
+            return res.status(422).json({error: "Email already exists!"});
+        
+        const user = new User({
+            name, email, phone, work, password, cpassword
+        });
+
+        user.save()
+        .then(() => {
+            res.status(201).json({message: "User Registered Successfully"});
+        })
+        .catch(err => res.status(500).json({error: "Failed to register"}));
+    })
+    .catch(err => {console.log(err)})
 });
 
 module.exports = router;

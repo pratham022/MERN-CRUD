2fedc3dcc6581f4b71f25d13db8207b22a2f1b0f
pratham022 <pthombre657@gmail.com>
2021-11-21 23:35:17 +0530

    Implement basic routing using React Router DOM

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
+import { Routes, Route } from "react-router-dom";
+
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Login from './components/Login';
+import Signup from './components/Signup';
 
 function App() {
   return (
     <>
       <Navbar />
+
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+        <Route exact path="/about" element={<About/>} />
+        <Route exact path="/contact" element={<Contact/>} />
+        <Route exact path="/login" element={<Login/>} />
+        <Route exact path="/signup" element={<Signup/>} />
+      </Routes>
     </>
   );
 }

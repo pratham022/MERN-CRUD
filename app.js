26b7ad1f9e31badaef0d9b9b3322d81892b94e19
pratham022 <pthombre657@gmail.com>
2022-03-29 13:11:15 +0530

    change file structure

--- app.js
+++ app.js
@@ -18,6 +18,17 @@ const middleware = (req, res, next) => {
     next();
 }
 
+// // ... other imports 
+// const path = require("path")
+
+// // ... other app.use middleware 
+// app.use(express.static(path.join(__dirname, "client", "build")))
+
+// // ...
+// // Right before your app.listen(), add this:
+// app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+// });
 
 
 app.listen(PORT, () => {
